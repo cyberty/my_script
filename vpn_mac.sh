3f7ff12ea4c20506dacc1d8baa98b5332282754b
cyberty <cyberty@gmail.com>
2014-06-19 23:12:52 +0800

    bug fix to producttion release

--- vpn_mac.sh
+++ vpn_mac.sh
@@ -4,17 +4,37 @@ function isnt_connected () {
     scutil --nc status "$vpn" | head -1 | grep -qv Connected
 }
 
+function isnt_disconnected () {
+    scutil --nc status "$vpn" | head -1 | grep -qv Disconnected
+}
 
-vpnlist=('VPN1' 'VPN2' 'VPN3')
+#vpnlist=('云梯 新加坡1号 L2TP')
+vpnlist=('云梯 新加坡1号 L2TP' '云梯 新加坡2号 L2TP' '云梯 台湾1号 L2TP' '云梯 香港1号 L2TP' '云梯 香港2号 L2TP')
 for ((i=0; i < ${#vpnlist[*]}; i++)); do
 	vpn=${vpnlist[$i]}
     echo ${vpn} "Connecting..."
     scutil --nc start "${vpn}" --secret 'VPNCloudMakesVPNEasy' 
     let loops=0
+    let maxloops=15
     while isnt_connected "$vpn"; do
-    	sleep 1
-    	let loops=$loops+1
-    	[ $loops -gt 10 ] && break
+        sleep 1
+        let loops=$loops+1
+        [ $loops -gt $maxloops ] && break
     done
-    [ $loops -lt 10 ] && networksetup -setdnsservers "${vpn}" 192.168.100.1 10.0.1.1
+
+    if [ $loops -le $maxloops ]; then
+        networksetup -setdnsservers "${vpn}" 192.168.100.1 10.0.1.1
+        echo "Success!"
+        exit
+    else
+        scutil --nc stop "${vpn}"
+        let loops=0
+        let maxloops=20
+        while isnt_disconnected "$vpn"; do
+            sleep 1
+            let loops=$loops+1
+            [ $loops -gt $maxloops ] && break
+        done
+    fi
+    
 done
